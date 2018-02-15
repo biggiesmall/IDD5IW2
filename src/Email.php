0697b9c542055b76cd6cf6abe46e5801cf920726
Abdel-latif <amatroud0@hotmail.com>
2018-02-15 22:01:38 +0100

    add travis

---
+++ src/Email.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdel-latifmabrouck
+ * Date: 15/02/2018
+ * Time: 21:54
+ */
+
+declare(strict_types=1);
+
+final class Email
+{
+    private $email;
+
+    private function __construct(string $email)
+    {
+        $this->ensureIsValidEmail($email);
+
+        $this->email = $email;
+    }
+
+    public static function fromString(string $email): self
+    {
+        return new self($email);
+    }
+
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+
+    private function ensureIsValidEmail(string $email): void
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid email address',
+                    $email
+                )
+            );
+        }
+    }
+}
