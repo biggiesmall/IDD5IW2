4c5577f24fe3458e29d76a3df94b830ae6b96d01
Abdel-latif <amatroud0@hotmail.com>
2018-02-16 13:09:01 +0100

    add travis

--- index.php
+++ index.php
@@ -1,12 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: abdel-latifmabrouck
- * Date: 16/02/2018
- * Time: 12:03
- */
-Class Index {
-    public function index(){
-        echo "<h1>Hello</h1>h1>";
-    }
-}
+echo "<h1>Hello</h1>h1>";
